82dbbca95b4f5c093beb72fb5824b6760ae5eb1c
CodeXTF2 <ethanseow2002@gmail.com>
2023-03-27 22:06:09 +0800

    iwan2die

--- INF1005Project/processes/products.php
+++ INF1005Project/processes/products.php
@@ -1,7 +1,8 @@
-<?php>
-$config = parse_ini_file('../../private/db-config.ini');
+<?php
+$config = parse_ini_file('../../../private/db-config.ini');
 $conn = new mysqli($config['servername'], $config['username'],
 $config['password'], $config['dbname']);
+
 function getProductById($id) {
   global $conn;
   // Prepare the SQL statement
@@ -76,9 +77,14 @@ function createProduct($name, $price, $image, $category, $stock, $description) {
 
 
 function searchProductsByName($searchTerm) {
+  
   global $conn;
-  $sql = "SELECT * FROM products WHERE name LIKE '%" . $searchTerm . "%'";
-  $result = $conn->query($sql);
+  $sql = "SELECT * FROM products WHERE name LIKE ?";
+  $stmt = $conn->prepare($sql);
+  $searchTerm = '%' . $searchTerm . '%';
+  $stmt->bind_param('s', $searchTerm);
+  $stmt->execute();
+  $result = $stmt->get_result();
   if ($result->num_rows > 0) {
     $products = array();
     while ($row = $result->fetch_assoc()) {
@@ -88,5 +94,80 @@ function searchProductsByName($searchTerm) {
   } else {
     return null;
   }
+}
+
+
+function searchProductsByCategory($searchTerm) {
+  
+  global $conn;
+  $sql = "SELECT * FROM products WHERE category = ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param('s', $searchTerm);
+  $stmt->execute();
+  $result = $stmt->get_result();
+  if ($result->num_rows > 0) {
+    $products = array();
+    while ($row = $result->fetch_assoc()) {
+      $products[] = $row;
+    }
+    return $products;
+  } else {
+    return null;
+  }
+}
+
+if(isset($_POST['action'])) {
+  $action = $_POST['action'];
+  // Call the corresponding function based on the action
+  switch ($action) {
+    case 'getProductById':
+      $id = $_POST['id'];
+      $product = getProductById($id);
+      // Output the product in JSON format
+      echo json_encode($product);
+      break;
+
+    case 'updateProduct':
+      $id = $_POST['id'];
+      $name = $_POST['name'];
+      $price = $_POST['price'];
+      $image = $_POST['image'];
+      $category = $_POST['category'];
+      $stock = $_POST['stock'];
+      $description = $_POST['description'];
+      updateProduct($id, $name, $price, $image, $category, $stock, $description);
+      break;
+
+    case 'deleteProduct':
+      $id = $_POST['id'];
+      deleteProduct($id);
+      break;
+
+    case 'createProduct':
+      $name = $_POST['name'];
+      $price = $_POST['price'];
+      $image = $_POST['image'];
+      $category = $_POST['category'];
+      $stock = $_POST['stock'];
+      $description = $_POST['description'];
+      createProduct($name, $price, $image, $category, $stock, $description);
+      break;
+
+    case 'searchProductsByName':
+      $searchTerm = $_POST['searchTerm'];
+      $products = searchProductsByName($searchTerm);
+      // Output the products in JSON format
+      echo json_encode($products);
+      break;
+
+    case 'searchProductsByCategory':
+      $searchTerm = $_POST['searchTerm'];
+      $products = searchProductsByCategory($searchTerm);
+      // Output the products in JSON format
+      echo json_encode($products);
+      break;
+
+  
+}
 }
 ?>
