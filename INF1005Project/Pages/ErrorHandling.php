223994e0086da8ef4f78e88df45e94d13e6909a2
2202433 <122218696+ivanpyw1999@users.noreply.github.com>
2023-03-28 03:33:58 +0800

    added about us page, contact us page, ErrorHandling Page

---
+++ INF1005Project/Pages/ErrorHandling.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this template
+-->
+<html lang="en">
+    <head>
+        <link rel="stylesheet"
+              href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+              integrity=
+              "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+              crossorigin="anonymous">
+
+        <link rel="stylesheet" href="../css/ErrorHandling.css">
+
+        <!--jQuery-->
+        <script defer
+                src="https://code.jquery.com/jquery-3.4.1.min.js"
+                integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+                crossorigin="anonymous">
+        </script>
+        <!--Bootstrap JS-->
+        <script defer
+                src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
+                integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
+                crossorigin="anonymous">
+        </script>
+        <!-- Custom JS -->
+        <script defer src="../js/ErrorHandling.js"></script>
+
+        <title>FastFash</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+
+    <body class = "d-flex flex-column min-vh-100">
+        <?php
+        include "nav.inc.php";
+        ?>
+
+
+
+        <main class ="container">
+            <section class="centered">
+                <h1>500 Server Error</h1>
+                    <span class="message" id="js-whoops"></span> <span class="message" id="js-appears"></span> 
+                    <span class="message" id="js-error"></span> <span class="message" id="js-apology"></span>
+                    <div>
+                        <span class="hidden" id="js-hidden">Message Here</span>
+                    </div>
+            </section>
+        </main>
+
+
+
+        <?php
+        include "footer.inc.php";
+        ?>
+
+    </body>
+
+</html>
