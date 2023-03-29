da4b7db1ad9e7adb1f3d073be3bc90c78b2d541e
itsalys <rab.alyxxa@gmail.com>
2023-03-30 06:40:54 +0800

    my account ui + order history + specific product

--- INF1005Project/js/myAccount.js
+++ INF1005Project/js/myAccount.js
@@ -3,26 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
  */
 
-function myFunction(clicked_button) {
-//  alert(clicked_button);
-    switch (clicked_button) {
-        case  "personal-information-edit-button":
-        case  "personal-information-view-button":
-            $("#personal-information-edit").toggleClass('d-none');
-            $("#personal-information-view").toggleClass('d-none');
-            break;
+function checkpwd() {
+    var password = document.getElementById("pwd")
+            , confirm_password = document.getElementById("pwd_confirm");
 
-        case  "delivery-location-edit-button":
-        case  "delivery-location-view-button":
-            $("#delivery-location-edit").toggleClass('d-none');
-            $("#delivery-location-view").toggleClass('d-none');
-            break;
+    function validatePassword() {
+        if (password.value !== confirm_password.value) {
+            confirm_password.setCustomValidity("Passwords Don't Match");
+        } else {
+            confirm_password.setCustomValidity('');
+        }
+    }
 
-        case  "change-password-edit-button":
-        case  "change-password-check-button":
-            $("#change-password-edit").toggleClass('d-none');
-            $("#change-password-check").toggleClass('d-none');
-            break;
+    password.onchange = validatePassword;
+    confirm_password.onkeyup = validatePassword;
+}
 
+function enableInputs(clicked_button) {
+    
+    switch(clicked_button) {
+        case    "personal-information-view-button":
+                    alert("test");
+                    $(".personalInfoButton").toggleClass('d-none');
+                    var inputs = document.getElementsByClassName('personalInfo');
+                    break;
+        case    "delivery-location-view-button":
+                    $(".deliverylocationButton").toggleClass('d-none');
+                    var inputs = document.getElementsByClassName('deliverylocation');
+                    break;
+    }
+    
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = false;
     }
 }
